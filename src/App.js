9d22a886c3085bc1388fbafa0e28531e7d20bee5
manishMandal02 <manishmandalj@gmail.com>
2021-06-03 11:22:35 +0530

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import ContentBox from './Components/LandingPage/ContentBox';
-import { Route, Router, Switch } from 'react-router-dom';
-import Guidelines from './Components/Pages/Guidelines';
 
 function App() {
   return (
     <div className='App'>
-      <Switch>
+      {/* <Switch>
         <Route path='/guidelines'>
           <Guidelines />
         </Route>
-        <Route path='/' exact>
-          <ContentBox />
-          <Footer />
-        </Route>
-      </Switch>
+        <Route path='/' exact> */}
+      <ContentBox />
+      <Footer />
+      {/* </Route>
+      </Switch> */}
     </div>
   );
 }
